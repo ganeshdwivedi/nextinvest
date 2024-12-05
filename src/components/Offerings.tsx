ca66fd5ea3286bb609faf7a2e5f947b13537fd60
ganeshdwivedi <ganeshdwivedi1783@gmail.com>
2024-12-05 11:11:56 +0530

    updated

--- src/components/Offerings.tsx
+++ src/components/Offerings.tsx
@@ -26,7 +26,7 @@ const Offerings = () => {
         Explore pre-vetted investment opportunites available in a growing number
         of industry categories
       </h6>
-      <img src={"/images/shape.svg"} className="absolute w-52 top-52 left-10" />
+      <img src={"/images/shape.svg"} alt='img' className="absolute w-52 top-52 left-10" />
       <div className="grid grid-cols-3 gap-5  my-7">
         {allInvestments?.length > 0 ? (
           allInvestments.map((item: any, index: number) => (
